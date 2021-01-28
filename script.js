d1870b45b0730bc657955b5ee09d4dbee661411d
jasminelychan <77300672+jasminelychan@users.noreply.github.com>
2021-01-28 01:15:03 -0500

    add javascript for exercise10

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("this is jasmine's 2nd project about time");
+
+// $(".routine").click(function(){
+//     $("routine").css("color","red");
+// });
+
+// $("p").css("color","red");
+
+// let routine=document.getElementById("routine");
+// routine.style.color="green";
+
+//  routine.addEventListener("click", function(){
+//      routine.style.color="red";
+//     });
+
+
+
+
+// $("#scale1").click(function(){
+//     $("#scale1").css("width", "60%");
+    
+// });
+
+$("button").css({
+    "background-color": "white",
+    "font-size": "0.8rem",
+    "padding": "5px",
+    "border-radius": "50%",
+    }
+    );
+
+$("button").click(function(){
+    $("body").toggleClass("colorChange");    
+    });
+
+
+
